8b182c95e753a9a2c5f5286077f9d70c121039bf
Josh Long <josh@joshlong.com>
2018-08-28 12:28:07 -0700

    bd

---
+++ before_deploy.sh
@@ -0,0 +1,6 @@
+tag=$(date +'%Y%m%d%H%M%S')-$(git log --format=%h -1)
+echo "the release tag will be ${tag}..."
+git config --local user.name "${GIT_NAME}"
+git config --local user.email "${GIT_EMAIL}"
+git tag "$tag"
+#git push origin "$tag"
